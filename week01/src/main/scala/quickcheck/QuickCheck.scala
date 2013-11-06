6c0687ee62039700f1f51a3079b7f5036ae3ca23
james_maggs <james.maggs@db.com>
2013-11-06 12:15:06 +0000

    First attempt. Everything is green but the properties are a bit lame.

--- week01/src/main/scala/quickcheck/QuickCheck.scala
+++ week01/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,7 +1,5 @@
 package quickcheck
 
-import common._
-
 import org.scalacheck._
 import Arbitrary._
 import Gen._
@@ -14,8 +12,45 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+    findMin(h) == a.min(b)
+  }
+
+  property("delete min") = forAll { a: Int =>
+    val h = deleteMin(insert(a, empty))
+    isEmpty(h)
+  }
+
+  property("delete min with two") = forAll { (a: Int, b: Int) =>
+    val h = insert(b, insert(a, empty))
+    findMin(deleteMin(h)) == a.max(b)
+  }
+
+  property("ordering") = forAll { h: H =>
+    val xs = elements(h)
+    xs == xs.sorted
+  }
+
+  property("contents") = forAll { (a: Int, b: Int, c: Int) =>
+    val h = insert(c, insert(b, insert(a, empty)))
+    val xs = elements(h)
+    xs.contains(a) && xs.contains(b) && xs.contains(c)
+  }
+
+  property("heap meld") = forAll { (h1: H, h2: H) =>
+    val min = findMin(meld(h1, h2))
+    min == findMin(h1) || min == findMin(h2)
+  }
+
+  def elements(heap: H): List[A] =
+    if(isEmpty(heap)) Nil
+    else findMin(heap) :: elements(deleteMin(heap))
+
+  lazy val genHeap: Gen[H] = for {
+    a <- arbitrary[Int]
+    h <- oneOf(value(empty), genHeap)
+  } yield insert(a, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
-
 }
