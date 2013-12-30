34321158e42e3c7016aa05dc983ae7af04a7ef55
James Maggs <github@jamesmaggs.com>
2013-12-30 14:49:44 +0000

    Step 3 passing.

--- week06/src/main/scala/kvstore/Replicator.scala
+++ week06/src/main/scala/kvstore/Replicator.scala
@@ -1,8 +1,6 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
+import akka.actor.{Cancellable, Props, Actor, ActorRef}
 import scala.concurrent.duration._
 
 object Replicator {
@@ -25,7 +23,7 @@ class Replicator(val replica: ActorRef) extends Actor {
    */
 
   // map from sequence number to pair of sender and request
-  var acks = Map.empty[Long, (ActorRef, Replicate)]
+  var acks = Map.empty[Long, (ActorRef, Replicate, Cancellable)]
   // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
   var pending = Vector.empty[Snapshot]
   
@@ -35,9 +33,26 @@ class Replicator(val replica: ActorRef) extends Actor {
     _seqCounter += 1
     ret
   }
-  
+
+  def acknowledgeReplication(tuple: (ActorRef, Replicate, Cancellable)) = tuple match {
+    case (actor, msg, cancellable) => {
+      cancellable.cancel()
+      actor ! Replicated(msg.key, msg.id)
+    }
+  }
+
   /* TODO Behavior for the Replicator. */
   def receive: Receive = {
+    case msg: Replicate => {
+      val seq = nextSeq
+      val snapshot = Snapshot(msg.key, msg.valueOption, seq)
+      val cancellable = context.system.scheduler.schedule(0 millis, 100 millis, replica, snapshot)
+      acks = acks.updated(seq, (sender, msg, cancellable))
+    }
+    case SnapshotAck(key, seq) => {
+      acks.get(seq).foreach(acknowledgeReplication)
+      acks = acks - seq
+    }
     case _ =>
   }
 
