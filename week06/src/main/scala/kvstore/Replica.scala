0fe7b5ad79976831bd5a9be1c3e7ceb89f28fb55
James Maggs <github@jamesmaggs.com>
2013-12-30 17:21:28 +0000

    Step 5 in progress.

--- week06/src/main/scala/kvstore/Replica.scala
+++ week06/src/main/scala/kvstore/Replica.scala
@@ -1,17 +1,14 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import akka.actor._
 import kvstore.Arbiter._
 import scala.collection.immutable.Queue
 import akka.actor.SupervisorStrategy.Restart
 import scala.annotation.tailrec
 import akka.pattern.{ ask, pipe }
-import akka.actor.Terminated
 import scala.concurrent.duration._
-import akka.actor.PoisonPill
-import akka.actor.OneForOneStrategy
-import akka.actor.SupervisorStrategy
 import akka.util.Timeout
+import scala.Some
 
 object Replica {
   sealed trait Operation {
@@ -39,12 +36,18 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
-  
+
+  var persistence = context.system.actorOf(persistenceProps)
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
+
+  var unpersistedOperations = Map.empty[Long, (ActorRef, Operation, Cancellable)]
+  var unpersistedSnapshots = Map.empty[Long, (ActorRef, Snapshot, Cancellable)]
+
 
   def add(key: String, value: String) = { kv = kv.updated(key, value) }
 
@@ -62,18 +65,32 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
-    case Insert(key, value, id) => {
-      add(key, value)
-      sender ! OperationAck(id)
+    case msg: Insert => {
+      add(msg.key, msg.value)
+      persistInsert(sender, msg)
     }
-    case Remove(key, id) => {
-      remove(key)
-      sender ! OperationAck(id)
+    case msg: Remove => {
+      remove(msg.key)
+      persistRemove(sender, msg)
     }
     case Get(key, id) => {
       sender ! GetResult(key, kv.get(key), id)
     }
+    case Persisted(key, id) => {
+      unpersistedOperations.get(id).foreach(acknowledgeOperation)
+      unpersistedOperations = unpersistedOperations - id
+    }
     case _ =>
+  }
+
+  def persistInsert(sender: ActorRef, msg: Insert) = {
+    val cancellable = context.system.scheduler.schedule(0 millis, 100 millis, persistence, Persist(msg.key, Some(msg.value), msg.id))
+    unpersistedOperations = unpersistedOperations.updated(msg.id, (sender, msg, cancellable));
+  }
+
+  def persistRemove(sender: ActorRef, msg: Remove) = {
+    val cancellable = context.system.scheduler.schedule(0 millis, 100 millis, persistence, Persist(msg.key, None, msg.id))
+    unpersistedOperations = unpersistedOperations.updated(msg.id, (sender, msg, cancellable));
   }
 
   var expected: Long = 0
@@ -83,17 +100,38 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
     case Get(key, id) => {
       sender ! GetResult(key, kv.get(key), id)
     }
-    case Snapshot(key, valueOption, seq) => {
-      if(seq == expected) {
-        update(key, valueOption)
+    case msg: Snapshot => {
+      if(msg.seq == expected) {
+        update(msg.key, msg.valueOption)
+        val cancellable = context.system.scheduler.schedule(0 millis, 100 millis, persistence, Persist(msg.key, msg.valueOption, msg.seq))
+        unpersistedSnapshots = unpersistedSnapshots.updated(msg.seq, (sender, msg, cancellable))
       }
 
-      if(seq <= expected) {
-        sender ! SnapshotAck(key, seq)
-        expected = math.max(expected, seq + 1)
+      if(msg.seq < expected) {
+        sender ! SnapshotAck(msg.key, msg.seq)
+        expected = math.max(expected, msg.seq + 1)
       }
     }
+    case Persisted(key, seq) => {
+      unpersistedSnapshots.get(seq).foreach(acknowledgeSnapshot)
+      unpersistedSnapshots = unpersistedSnapshots - seq
+      expected = math.max(expected, seq + 1)
+    }
     case _ =>
+  }
+
+  def acknowledgeOperation(tuple: (ActorRef, Operation, Cancellable)) = tuple match {
+    case (actor, msg, cancellable) => {
+      actor ! OperationAck(msg.id)
+      cancellable.cancel()
+    }
+  }
+
+  def acknowledgeSnapshot(tuple: (ActorRef, Snapshot, Cancellable)) = tuple match {
+    case (actor, msg, cancellable) => {
+      actor ! SnapshotAck(msg.key, msg.seq)
+      cancellable.cancel()
+    }
   }
 
   arbiter ! Join
